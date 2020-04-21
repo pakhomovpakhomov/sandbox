a864a2d0e053d06ae72f55cb4d418280ab5adde8
Ivan Pakhomov <pakhomovivaneduardovich@gmail.com>
2020-04-21 14:49:38 +0500

    fix

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include "lib.h"
 
-#include <spdlog/spdlog.h>
+#include <iostream>
 
 int main()
 {
-    auto logger = spdlog::stdout_logger_mt("console");
-    logger->info("version {} was started", version());
+    std::cout << "build" << version();
+    std::cout << "\nHello, World!";
 
     return 0;
 }
